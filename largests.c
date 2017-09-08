e6e7ce709b45385e6c0c32e33d7b1c64b2b1252a
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2017-09-08 19:01:32 +0530

    Rename largest.c to largests.c

---
+++ largests.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is largest");
+}
+elseif(b>c)
+{
+printf(b is big");
+}
+else
+{
+printf("c is big");
+}
+}
