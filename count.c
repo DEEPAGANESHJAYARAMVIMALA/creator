a61148eb67dec3122122df16bc723cbc25846af5
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2017-09-12 15:29:52 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+printf("enter the number");
+scanf("%d",&i);
+while(n!=0)
+{
+n/=10;
+}
+printf("count the number of digits",+i)'
+}
