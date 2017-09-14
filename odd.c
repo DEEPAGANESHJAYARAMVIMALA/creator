0f3cf2d20289799c2a736d674c8587b7097d98e0
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2017-09-14 19:44:23 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int n,m =20,n=40;
+
+    clrscr();
+
+    printf("Print Odd Numbers in a given range:\n");
+
+    for (n = m; n <= n; n++)
+
+        {
+
+               if (n % 2 == 1)
+
+                  printf ("%d ", n);
+
+         }
+
+                getch();
+
+} 
