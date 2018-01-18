7d4a302221b662561dcf56bfe8ef1899c3761e6d
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-01-18 16:51:04 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],n,min,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            printf("%d",max);
+        }
+    }
+}
