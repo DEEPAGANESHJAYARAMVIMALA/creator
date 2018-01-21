9940f825fc9959267b6ad304d9d6075d9f038575
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-01-21 09:51:01 +0530

    Create hr:min.c

---
+++ hr:min.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int hr,min,rr,hr1,min1,rr1;
+printf("enter the hr and min");
+scanf("%d %d",&hr,&min);
+printf("enter the hr and min");
+scanf("%d %d",&hr1,&min1);
+rr=hr-hr1;
+rr1=min-min1;
+printf("%d %d",rr,rr1);
+}
