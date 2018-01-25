869ed4fcc81b9d7073590529c76d32c67584af17
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-01-25 16:18:43 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    char str[100];
+    int count=0;
+    int i;
+    printf("enter the string");
+    gets(str);
+    for(i=0;str[i]!==null;i++)
+    {
+        if(a[i]==" ")
+        {
+            count++;
+           
+        }
+         printf("count the number of characters:%d",count);
+    }
+}
