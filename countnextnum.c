bdabe2691c8ae1a5ffad764e2e78595faf0cbd9b
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-01 08:56:17 +0000

    Create countnextnum.c

---
+++ countnextnum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int n,count=1,i;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        
+        count++;
+           
+    }
+     printf("the count string is :%d",count+1);      
+}
