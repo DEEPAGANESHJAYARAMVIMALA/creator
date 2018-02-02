ca275da3d45b8e9aec64f445e4655486bcdf5d83
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-02 14:43:26 +0530

    Create poweryesorno.c

---
+++ poweryesorno.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n=pow(2,n/2))
+    {
+        printf("yes");
+        
+    }
+    else("no");
+    
+}
