c7d24787a1e72d5d97d9b38dbbd731ddece337ea
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-06 11:22:36 +0000

    Create pow1.c

---
+++ pow1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n=pow(2,n/2))
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
