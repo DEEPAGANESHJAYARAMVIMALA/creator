733588f8a7c921ff07d92f4a4d973ab557db6966
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-06 10:43:21 +0000

    Create cmp.c

---
+++ cmp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str1[25],str2[25];
+    printf("enter the  first string");
+scanf("%s",&str1);
+    printf("enter the second string");
+ scanf("%s",&str2);
+   printf("%s%s",str1,str2);
+}
+
+
