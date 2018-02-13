af3d0f04d8f44f651414fcbfe7f31ce8a34ec9f6
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-13 16:51:16 +0530

    Create alphabetsorinteger.c

---
+++ alphabetsorinteger.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+char c[200];
+int count=0,i;
+printf("Enter the paragraph\n");
+scanf("%[^\n]s",c);
+for(i=0;c[i]!='\0';i++)
+{
+if((c[i]=='0')||(c[1]==1)||(c[i]=='2')||(c[i]=='3')||(c[i]=='4')||(c[i]=='5')||(c[i]=='6')||(c[i]=='7')||(c[i]=='7')||
+(c[i]=='8')||(c[i]=='9'))
+count++;
+}
+
+if(c==c)
+{
+    printf("yes");
+    
+}
+else
+{
+    printf("no");
+}
+}
