79c6a2ea12c147744caad54c68075cbff563e01b
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-02-13 16:02:26 +0530

    Create 1to10.c

---
+++ 1to10.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the number");
+    scanf("%d",&a);
+    switch(a)
+    {
+         case 1:printf("ONE");
+                break;
+         case 2:printf("TWO");
+                break;
+         case 3:printf("THREE");
+                break;
+         case 4:printf("FOUR");
+                break;
+         case 5:printf("FIVE");
+                break;
+        case 6:printf("SIX");
+                break;
+        case 7:printf("SEVEN");
+                break;
+        case 8:printf("EIGHT");
+                break;
+        case 9:printf("NINE");
+                break;
+        case 10:printf("TEN");
+                break;
+        default :printf("enter the limit with in 1 to 10");
+                
+    }
+}
+
