87d8b61ef7e1ac4ab1cbf6d8c16fbcad75e027c2
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-03-01 15:42:00 +0530

    Create 1to65.c

---
+++ 1to65.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    scanf("%d%d",&a,&b);
+    if(n>a&&n<6)
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+    }
+}
