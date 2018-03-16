0794b1ae24d3d9571a920026c94b62b9bb9e1745
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-03-16 14:53:47 +0530

    Create celsius.c

---
+++ celsius.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+#include<math.h>
+int main()
+{
+    float kel,cel;
+printf("enter temperatue in celcius");
+scanf("%f",&cel);
+kel=cel+273.15;
+printf("kelvin = %.0f",kel);
+
+    return 0;
+}
