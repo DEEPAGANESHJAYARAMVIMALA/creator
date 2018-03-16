c69eef9a763cd47c2876ed193da6b94635598623
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-03-16 14:59:09 +0530

    Create simpleinterest.c

---
+++ simpleinterest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+ int p,n,r,simpleinterest;
+printf("enter the vale of p n r:");
+scanf("%d%d%d",&p,&n,&r);
+simpleinterest=(p*n*r)/100;
+printf("simpleinterestvis:%d",simpleinterest);
+
+    return 0;
+}
